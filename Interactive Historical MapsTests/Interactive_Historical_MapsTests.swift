775af8abc5996d5b24e419479978904e2fe3bf7a
Gokulan Gnanendran <gxg5186@psu.edu>
2018-11-02 18:45:45 -0400

    historical maps project

---
+++ Interactive Historical MapsTests/Interactive_Historical_MapsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Interactive_Historical_MapsTests.swift
+//  Interactive Historical MapsTests
+//
+//  Created by Goki on 11/2/18.
+//  Copyright © 2018 Gokulan Gnanendran. All rights reserved.
+//
+
+import XCTest
+@testable import Interactive_Historical_Maps
+
+class Interactive_Historical_MapsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
