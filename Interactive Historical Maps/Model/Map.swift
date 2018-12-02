45f250f3f5647ecec61da51cf5d073fe4cdd7f37
Gokulan Gnanendran <gxg5186@psu.edu>
2018-12-02 18:17:08 -0500

    ArcGIS SDK is no good

---
+++ Interactive Historical Maps/Model/Map.swift
@@ -0,0 +1,9 @@
+//
+//  Map.swift
+//  Interactive Historical Maps
+//
+//  Created by Goki on 12/1/18.
+//  Copyright © 2018 Gokulan Gnanendran. All rights reserved.
+//
+
+import Foundation
---
+++ Interactive Historical Maps/Model/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  Interactive Historical Maps
+//
+//  Created by Goki on 12/1/18.
+//  Copyright © 2018 Gokulan Gnanendran. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    static let shared = Model()
+    
+    let maps : [Map]
+    var date : HistoricalDate
+    
+    private init() {
+        maps = []
+        date = HistoricalDate.init(month: 0, year: 0)
+    }
+}
