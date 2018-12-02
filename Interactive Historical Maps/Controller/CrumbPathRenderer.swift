bf35c0acbac5d7b84ccf2965a30e8fc3382cdb9f
Gokulan Gnanendran <gxg5186@psu.edu>
2018-12-02 18:07:47 -0500

    attempts to improve path rendering

---
+++ Interactive Historical Maps/Controller/CrumbPathRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  CrumbPathRenderer.swift
+//  Interactive Historical Maps
+//
+//  Created by Goki on 12/2/18.
+//  Copyright © 2018 Gokulan Gnanendran. All rights reserved.
+//
+
+import Foundation
---
+++ Interactive Historical Maps/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Interactive Historical Maps
+//
+//  Created by Goki on 12/1/18.
+//  Copyright © 2018 Gokulan Gnanendran. All rights reserved.
+//
+
+import Foundation
